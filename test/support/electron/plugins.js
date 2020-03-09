5f47928cf20bdea76bd866df95deb89a8ce01fe5
Grzegorz Wozniak <grzegorz.wozniak@coveainsurance.co.uk>
2020-03-09 11:37:20 +0000

    Reverting DevTools to be remote

--- test/support/electron/plugins.js
+++ test/support/electron/plugins.js
@@ -1,15 +1,9 @@
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true
 
 require('./electronReload')
+require('./electronDevTools')
 
 const { app } = require('electron')
-
-app.on('ready', (e) => {
-  // addDevToolsExtension
-  const electron = require('electron')
-  const BrowserWindow = electron.BrowserWindow
-  BrowserWindow.addDevToolsExtension('/home/gwozniak/.config/google-chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.4.0_0')
-})
 
 app.on('before-quit', function () {
   app.exit()
